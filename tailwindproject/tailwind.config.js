2458285b6743398bdd4d781e870665fa69728b4f
iremcetiner1 <biiremcetiner@gmail.com>
2024-05-20 17:30:30 +0300

    changes

--- tailwindproject/tailwind.config.js
+++ tailwindproject/tailwind.config.js
@@ -1,29 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ['./public/**/*.{html,js}'],
+  darkMode: 'class',
   theme: {
-    container:{
-      center:true,
-      screens:{
-        lg:'1140px',
-        xl:'1140px',
-        '2xl':'1140',
-      }
+    container: {
+      center: true,
+      screens: {
+        lg: '1140px',
+        xl: '1140px',
+        '2xl': '1140px',
+      },
     },
     extend: {
-      fontFamily:{
-        gemunu : ["Gemunu Libre", 'sans-serif'],
-        open : ['Open Sans','sans-serif']
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif'],
       },
+
       colors: {
-        'gega-red':'#BC1A45',
-        'gega-melon':'FFD369',
-        'gega-grey' : '#DDDDDD',
+        'gega-red': '#BC1A45',
+        'gega-melon': '#FFD369',
+        'gega-grey': '#DDDDDD',
         'gega-white': '#F7F7F7',
-      }
-    
+      },
+
+      spacing: {
+        128: '32rem',
+      },
     },
   },
   plugins: [],
-}
-
+};
